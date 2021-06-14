fd6ec1e8ff5dc3b4a47f7cec0c8b391791697551
Daniele <dan97gaming@gmail.com>
2021-06-14 15:46:37 +0200

    Aggiornamento file

--- Cinematica Diretta/Cinematica_Diretta_Posizione.m
+++ Cinematica Diretta/Cinematica_Diretta_Posizione.m
@@ -1,5 +1,5 @@
 function [P, theta3_new, theta4_new, E1, E2] = Cinematica_Diretta_Posizione(PKM, theta1, theta2,time)
-% Input: Posizione theta1, theta2 e thetaV e parametri robot
+% Input: Posizione theta1, theta2 e parametri robot
 % Output: Posizione End-effector, theta3, theta4, posizione E1 e E2
 d = PKM.link.d;
 l = PKM.link.l;
@@ -22,7 +22,7 @@ C = l^2*d^2*(cos(theta2(1,i))+cos(theta1(1,i)))^2 -l*d*(cos(theta2(1,i))+cos(the
 % Posizione Y end-effector
 y = (-B + sqrt(B^2 - 4*A*C))/(2*A);
 % Posizione X end-effector
-x = (y*l*(sin(theta2(1,i))-sin(theta1(1,i))) - l*d*(cos(theta2(1,i))+cos(theta1(1,i))))/(-2*d -l*(cos(theta2(1,i))-cos(theta1(1,i))));
+x = (y*l*(sin(theta2(1,i))-sin(theta1(1,i))) - l*d*(cos(theta2(1,i))+cos(theta1(1,i))))q/(-2*d -l*(cos(theta2(1,i))-cos(theta1(1,i))));
 
 
 % X punto E1
